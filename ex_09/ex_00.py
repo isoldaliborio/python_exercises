9600dc3e0e25440caf9178923051a55bef119b0d
isoldaliborio <isoldaliborio@gmail.com>
2022-10-22 22:51:44 +0100

    modifications and new exercices

--- ex_09/ex_00.py
+++ ex_09/ex_00.py
@@ -1,16 +1,19 @@
-ccc = dict()
+# ccc = dict()
 
-ccc['marquard'] = 1
-ccc['zhen'] = 1
-ccc['csev'] = 1
-ccc['zhen'] = 1
-ccc['cwen'] = 1
-print(ccc)
+# ccc['marquard'] = 1 
+# ccc['zhen'] = 1 
+# ccc['csev'] = 1 
+# ccc['zhen'] = 1
+# ccc['cwen'] = 1
+# print(ccc)
 
-ccc['marquard'] = ccc['marquard']+ 1
-ccc['zhen']  = ccc['zhen'] + 1 
-ccc['csev'] = ccc['csev'] + 1
-ccc['zhen'] = ccc['zhen'] + 1
-ccc['cwen'] = ccc['cwen'] + 1 
+# ccc['marquard'] = ccc['marquard']+ 1
+# ccc['zhen']  = ccc['zhen'] + 1 
+# ccc['csev'] = ccc['csev'] + 1
+# ccc['zhen'] = ccc['zhen'] + 1
+# ccc['cwen'] = ccc['cwen'] + 1 
 
 # So if the name comes in a imput for exemple the names will been add. 
+
+stuff = dict()
+print(stuff.get('candy',-1))
